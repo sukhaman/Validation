7e9908d2c9d09e6981c4adb14a4b0a961e940620
Sukhaman Singh <sukhamandev@gmail.com>
2024-01-15 17:09:33 -0500

    first commit

---
+++ Sources/Validation.swift
@@ -0,0 +1,45 @@
+//
+//  Created by Sukhaman Singh on 1/15/24.
+//
+
+import Foundation
+
+
+public enum ValidationResult {
+    case valid
+    case invalid(message: String)
+}
+
+public struct Validation {
+    
+    public static func validateUsername(_ username: String, minUsernameLength: Int = 4) -> ValidationResult {
+        // Customize the validation logic for the username
+        
+
+        if username.isEmpty {
+            return .invalid(message: "Username cannot be empty.")
+        } else if username.count < minUsernameLength {
+            return .invalid(message: "Username must be at least \(minUsernameLength) characters long.")
+        }
+
+        // Additional validation rules can be added
+
+        return .valid
+    }
+
+    public static func validatePassword(_ password: String, _ minPasswordLength: Int = 6) -> ValidationResult {
+        // Customize the validation logic for the password
+       
+        if password.isEmpty {
+            return .invalid(message: "Password cannot be empty.")
+        } else if password.count < minPasswordLength {
+            return .invalid(message: "Password must be at least \(minPasswordLength) characters long.")
+        }
+
+        // Additional validation rules can be added
+
+        return .valid
+    }
+
+    // Add more validation methods as needed
+}
